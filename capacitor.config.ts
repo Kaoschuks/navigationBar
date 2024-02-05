befb007b5741be4bbb0dead6be8857270fa33c62
Favour Chukwuemeka <kaosemeka@yahoo.com>
2024-02-05 21:38:39 +0100

    updated capacitor code

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,6 +3,8 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'navigationBar',
+	loggingBehavior: "none",
+	bundledWebRuntime: false,
   webDir: 'www',
   server: {
     androidScheme: 'https'
